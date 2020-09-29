2e924771d7f855273619e46f63a9cc62632efc62
Andrey Kartashov <deftbrain@gmail.com>
2020-09-29 11:04:58 +0200

    feat(client): add the 'Admin Panel ' link to the Welcome page

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {getUserData} from './authProvider';
+import {ADMIN_URL} from './config/app';
 
 export default () => (
   <div>
     <h1>Hello, {getUserData().name}!</h1>
     <ul>
+      <li><a href={ADMIN_URL} target="_blank">Admin Panel</a></li>
       <li><Link to="/program_increments/">Program Increments</Link></li>
     </ul>
   </div>
