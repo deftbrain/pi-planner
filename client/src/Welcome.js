3d5652f8fceabef31667b1adddc149993bda9bbf
Andrey Kartashov <deftbrain@gmail.com>
2021-01-08 12:59:03 +0200

    feat(client): use the Roboto font by default

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {getUserData} from './authProvider';
-import {ADMIN_URL} from './config/app';
+import './app.css';
 
 export default () => (
   <div>
     <h1>Hello, {getUserData().name}!</h1>
     <ul>
-      <li><a href={ADMIN_URL} target="_blank">Admin Panel</a></li>
       <li><Link to="/program_increments/">Program Increments</Link></li>
     </ul>
   </div>
