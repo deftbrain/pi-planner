9b03507e65d2728dc2e6e25ef5b10ba7dfa85b24
Andrey Kartashov <deftbrain@gmail.com>
2021-01-08 14:06:55 +0200

    feat(client): add a special icon to external links

--- client/src/components/entity/ExternalLink.js
+++ client/src/components/entity/ExternalLink.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Link from '@material-ui/core/Link';
+import LaunchIcon from '@material-ui/icons/Launch';
+import {makeStyles} from '@material-ui/core/styles';
+
+function stopPropagation(e) {
+  e.stopPropagation();
+}
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    fontSize: 'small',
+    padding: `0 ${theme.spacing(0.5)}px`,
+  }
+}))
 
 export default function (props) {
-  function stopPropagation(e) {
-    e.stopPropagation();
-  }
+  const classes = useStyles();
 
   return <Link href={props.entity['externalUrl']} target="_blank" onClick={stopPropagation} onFocus={stopPropagation}
-               aria-label="External link">
-    {props.entity['name']}
+               aria-label="External link">{props.entity['name']}<LaunchIcon className={classes.icon}/>
   </Link>
 }
