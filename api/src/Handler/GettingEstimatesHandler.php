17624b7fdcdcb1127e12fffe6f9ef010cfcb0c44
Andrey Kartashov <deftbrain@gmail.com>
2021-01-12 20:36:37 +0200

    Revert "fix(api): ignore estimates in epics that are not linked to a program increment anymore"
    This reverts commit f84cfb1b2d1060d207bcc7dfbf27925e019e5fd5.

--- api/src/Handler/GettingEstimatesHandler.php
+++ api/src/Handler/GettingEstimatesHandler.php
@@ -28,17 +28,10 @@ final class GettingEstimatesHandler
     public function __invoke(ProgramIncrement $programIncrement): array
     {
         $qb = $this->entityManager->createQueryBuilder();
-        $projectIds = [];
-        $epicIds = [];
-        foreach ($programIncrement->getProjectsSettings() as $ps) {
-            $projectIds[] = $ps->getProject()->getId();
-            $epicIds += $ps->getEpics()->map(fn ($epic) => $epic->getId())->getValues();
-        }
-
-        if (!$projectIds || !$epicIds) {
-            return [];
-        }
-
+        $projectIds = array_map(
+            fn ($ps) => $ps->getProject()->getId(),
+            $programIncrement->getProjectsSettings()->toArray()
+        );
         $result = $qb
             ->from(Workitem::class, 'w')
             ->select(
@@ -50,7 +43,6 @@ final class GettingEstimatesHandler
             )
             ->andWhere('w.isDeleted = false')
             ->andWhere($qb->expr()->in('w.project', $projectIds))
-            ->andWhere($qb->expr()->in('w.epic', $epicIds))
             ->andWhere($qb->expr()->orX('w.estimateFrontend > 0', 'w.estimateBackend > 0'))
             ->groupBy('w.epic', 'w.team', 'w.sprint')
             ->getQuery()
